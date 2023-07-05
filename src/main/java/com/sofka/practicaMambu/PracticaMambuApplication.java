b16e7c204df073034c771a00a2757337cad79fc4
Fernando Quiceno <fernando.quiceno@sofka.com.co>
2023-07-05 09:34:03 -0500

    Primer Commit

---
+++ src/main/java/com/sofka/practicaMambu/PracticaMambuApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.practicaMambu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaMambuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaMambuApplication.class, args);
+	}
+
+}
