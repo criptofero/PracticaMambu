335cda523ff36d0017a18f56943e3db665816eae
Andres Pinzón <andres.pinzon@sofka.com.co>
2023-07-05 23:46:20 -0500

    Se agrega header de autenticacion basica.

--- src/main/java/com/sofka/practicaMambu/infraestructure/ClientRepository.java
+++ src/main/java/com/sofka/practicaMambu/infraestructure/ClientRepository.java
@@ -7,6 +7,7 @@ import com.sofka.practicaMambu.domain.model.Client;
 import com.sofka.practicaMambu.domain.service.ClientService;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.http.HttpHeaders;
@@ -19,6 +20,8 @@ public class ClientRepository implements ClientService {
     @Override
     public ClientCreateResponseDTO createClient(Client client) {
         final String mambuAPIRootUrl = "https://sofkamambudev.sandbox.mambu.com/api";
+        final String MAMBU_API_USER = "andres.pinzon@sofka.com.co";
+        final String MAMBU_API_PASSWORD = "M4a8vS0tK.auJ62";
         String operationUrl = mambuAPIRootUrl.concat("/clients/");
         ObjectMapper mapper = new ObjectMapper();
         ClientCreateResponseDTO createResponse = new ClientCreateResponseDTO();
@@ -27,6 +30,7 @@ public class ClientRepository implements ClientService {
             jsonBody = mapper.writeValueAsString(client);
             UUID requestKey = UUID.randomUUID();
             org.springframework.http.HttpHeaders requestHeaders = new org.springframework.http.HttpHeaders();
+            requestHeaders.setBasicAuth(MAMBU_API_USER, MAMBU_API_PASSWORD);
             requestHeaders.set("Accept", "application/vnd.mambu.v2+json");
             requestHeaders.set("Content-Type", "application/json");
             requestHeaders.set("Idempotency-Key", requestKey.toString());
@@ -36,6 +40,9 @@ public class ClientRepository implements ClientService {
             createResponse.setFirstName("Bart");
             createResponse.setLastName("Simpson");
         } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (RestClientException e) {
+            System.err.println(e.toString());
             throw new RuntimeException(e);
         }
         return createResponse;
