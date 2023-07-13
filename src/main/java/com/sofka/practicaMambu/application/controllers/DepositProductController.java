1bdd3d624d06814cb90fb6afd634e75a4f1307c8
Andres Pinzón <andres.pinzon@sofka.com.co>
2023-07-13 17:33:13 -0500

    Se agrega validacion de fechas de consulta de movimientos

--- src/main/java/com/sofka/practicaMambu/application/controllers/DepositProductController.java
+++ src/main/java/com/sofka/practicaMambu/application/controllers/DepositProductController.java
@@ -1,17 +1,17 @@
 package com.sofka.practicaMambu.application.controllers;
 
-import com.sofka.practicaMambu.domain.dto.CreateDepositAccountResponse;
-import com.sofka.practicaMambu.domain.dto.CreateDepositTransactionResponse;
-import com.sofka.practicaMambu.domain.dto.DepositProductResponse;
-import com.sofka.practicaMambu.domain.dto.TransactionsQueryResponse;
+import com.sofka.practicaMambu.domain.dto.*;
 import com.sofka.practicaMambu.domain.model.DepositAccount;
 import com.sofka.practicaMambu.domain.model.DepositTransaction;
 import com.sofka.practicaMambu.domain.model.TransactionFilterInfo;
+import com.sofka.practicaMambu.domain.seedWork.CommonUtils;
 import com.sofka.practicaMambu.domain.service.DepositProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping("/depositproduct")
@@ -48,9 +48,42 @@ public class DepositProductController {
     }
 
     @GetMapping("/accounts/{accountKey}/transactions")
-    public ResponseEntity<TransactionsQueryResponse> getAccountTransactions(@RequestBody TransactionFilterInfo filterInfo){
-        TransactionsQueryResponse queryResponse = productService.getAccountTransactions(filterInfo);
-        ResponseEntity responseEntity = new ResponseEntity(queryResponse, queryResponse.getStatusCode());
-        return  responseEntity;
+    public ResponseEntity<TransactionsQueryResponse> getAccountTransactions(@RequestBody TransactionFilterInfo filterInfo) {
+        final String DATE_FORMAT = "yyyy-MM-dd";
+        final int MAX_QUERY_RANGE_DAYS = 30;
+        ResponseEntity responseEntity = null;
+        TransactionsQueryResponse queryResponse = null;
+        boolean isValidFilters = true;
+        var dateFilter = CommonUtils.getFirstFilterByFieldAndOperator(filterInfo.getFilterCriteria(), "creationDate", "BETWEEN");
+        if (dateFilter != null) {
+            var startDate = CommonUtils.parseDateString(dateFilter.getValue(), DATE_FORMAT);
+            var endDate = CommonUtils.parseDateString(dateFilter.getSecondValue(), DATE_FORMAT);
+            long queryRangeDays = startDate.until(endDate, ChronoUnit.DAYS);
+            MambuErrorResponse errorResponse = null;
+            if (startDate.compareTo(endDate) > 0) {
+                queryResponse = new TransactionsQueryResponse();
+                errorResponse = new MambuErrorResponse();
+                errorResponse.setErrorCode(1101);
+                errorResponse.setErrorReason("Fecha inicial debe ser menor a fecha final de consulta");
+                errorResponse.setErrorSource(dateFilter.getClass().getName());
+                queryResponse.setErrors(new MambuErrorResponse[]{errorResponse});
+                responseEntity = new ResponseEntity(queryResponse, HttpStatus.BAD_REQUEST);
+                isValidFilters = false;
+            }else if(queryRangeDays > MAX_QUERY_RANGE_DAYS){
+                queryResponse = new TransactionsQueryResponse();
+                errorResponse = new MambuErrorResponse();
+                errorResponse.setErrorCode(1102);
+                errorResponse.setErrorReason("Rango de consulta debe ser máximo de %d dias.".formatted(MAX_QUERY_RANGE_DAYS));
+                errorResponse.setErrorSource(dateFilter.getClass().getName());
+                queryResponse.setErrors(new MambuErrorResponse[]{errorResponse});
+                responseEntity = new ResponseEntity(queryResponse, HttpStatus.BAD_REQUEST);
+                isValidFilters = false;
+            }
+        }
+        if (isValidFilters) {
+            queryResponse = productService.getAccountTransactions(filterInfo);
+            responseEntity = new ResponseEntity(queryResponse, queryResponse.getStatusCode());
+        }
+        return responseEntity;
     }
 }
